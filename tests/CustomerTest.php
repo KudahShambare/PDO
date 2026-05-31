ef07ac470e8069b396bb4492d80d1a36532e60b6
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2026-05-31 06:15:10 +0200

    code chnages

--- tests/CustomerTest.php
+++ tests/CustomerTest.php
@@ -17,7 +17,7 @@ class CustomerTest extends TestCase
         $this->pdo = new PDO(
             "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']};charset=utf8",
             $_ENV['DB_USER'],
-            $_ENV['DB_PASS']
+            $_ENV['DB_PASS'],
         );
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
